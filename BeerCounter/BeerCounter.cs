8455ee6a263061ca8510f2fdaeaf28f5729a0617
Bruno Gomes <bruno.gomes.771@outlook.com>
2020-03-30 17:35:46 +0100

    Adicionado BeerCounter

---
+++ BeerCounter/BeerCounter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeerCounter
+{
+    class BeerCounter
+    {
+        public static int beerInStock = 0;
+        public static int beerDrankCount = 0;
+
+
+        public static void BuyBeer(int bottlesCount)
+        {
+            beerInStock += bottlesCount;
+        }
+
+        public static void DrinkBeer(int bottlesCount)
+        {
+            if (beerInStock != 0 && beerInStock >= bottlesCount)
+            {
+                beerInStock -= bottlesCount;
+                beerDrankCount += bottlesCount;
+            }
+            else //If there are less beers in stock than the ones being requested to drink, drink all available beers and turn stock to 0
+            {
+                beerDrankCount += beerInStock;
+                beerInStock = 0;
+            }
+        }
+    }
+}
---
+++ BeerCounter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BeerCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            int beersBought = 0;
+            int beersDrunk = 0;
+
+            while (command != "End")
+            {
+                string[] splits = command.Split(' ');
+                beersBought = Int32.Parse(splits[0]);
+                beersDrunk = Int32.Parse(splits[1]);
+
+                BeerCounter.BuyBeer(beersBought);
+                BeerCounter.DrinkBeer(beersDrunk);
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine("Beers remaining in Stock : {0}, Beers Drunk: {1}", BeerCounter.beerInStock, BeerCounter.beerDrankCount);
+        }
+    }
+}
