0deaa96c4d819293726200464a8520b292d6a566
Bruno Gomes <bruno.gomes.771@outlook.com>
2020-05-09 19:12:10 +0100

    (M1_WeaponSystem) Added IWeapon Interface and Weapon Base Classes

---
+++ M1_WeaponSystem/WeaponSystem/WeaponSystem/IWeapon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeaponSystem
+{
+    interface IWeapon
+    {
+        double BaseDamage { get; set; }
+        string WeaponName { get; set; }
+        double AdsTime { get; set; }
+        int AmmoCount { get; set; }
+        double ReloadSpeed { get; set; }
+
+        void Fire();
+        void Reload();
+        void ADS();
+        void Modify();
+    }
+}
