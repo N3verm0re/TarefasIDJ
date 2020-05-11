af2ad68d730ccd6518b499cbdce6fcf5605d2fc0
Bruno Gomes <bruno.gomes.771@outlook.com>
2020-05-11 05:21:59 +0100

    (M1_WeaponSystem) Added New Project ConfigureManufacturer

---
+++ M1_WeaponSystem/WeaponSystem/ConfigureManufacturer/ManufacturerBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConfigureManufacturer
+{
+    class ManufacturerBase
+    {
+        public Dictionary<string, double> sniperSightsMod;
+        public Dictionary<string, double> sightsMod;
+        public Dictionary<string, double> barrelMod;
+        public Dictionary<string, double> stockMod;
+        public Dictionary<string, double> magazineMod;
+    }
+}
---
+++ M1_WeaponSystem/WeaponSystem/ConfigureManufacturer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using WeaponSystem;
+
+namespace ConfigureManufacturer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type Load if you wish to load and edit an existing Json with Manufacturers or Type New to create a new list of Manufacturers. Type Exit to leave the program.");
+            string firstCommand = Console.ReadLine();
+
+            while(firstCommand.ToLower() != "exit")
+            {
+                if (firstCommand.ToLower() == "load")
+                {
+                    //TODO Implement loading of file 
+                    //TODO Implement editing of file
+                }
+                else if (firstCommand.ToLower() == "new")
+                {
+                    Dictionary<string, ManufacturerBase> manufacturerList = new Dictionary<string, ManufacturerBase>();
+                    //TODO Implement creation of Manufacturers
+                    //TODO Implement saving file
+                }
+                else
+                {
+                    Console.WriteLine("Invalid command, try again");
+                }
+
+                firstCommand = Console.ReadLine();
+            }
+        }
+    }
+}
