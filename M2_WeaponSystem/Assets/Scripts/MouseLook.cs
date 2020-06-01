306767359d98389e3cfd1b36abee5e1caf3dd695
Bruno Gomes <bruno.gomes.771@outlook.com>
2020-06-01 17:15:40 +0100

    Added M2 Unity Project

---
+++ M2_WeaponSystem/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float sensitivity = 100f;
+    public Transform playerBody;
+    float yRotation = 0f;
+    void Start()
+    {
+        //playerBody = GetComponentInParent<Transform>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        yRotation -= mouseY;
+        yRotation = Mathf.Clamp(yRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(yRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
