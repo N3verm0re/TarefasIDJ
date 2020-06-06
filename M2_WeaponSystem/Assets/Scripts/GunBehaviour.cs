209b4d6e7902113ae4f2f6b57741890cb5fdc809
Bruno Gomes <bruno.gomes.771@outlook.com>
2020-06-06 20:34:57 +0100

    (M2_WeaponSystem) Updated Unity Porject: Added Assets and Basic Behaviour Scripts

---
+++ M2_WeaponSystem/Assets/Scripts/GunBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunBehaviour : MonoBehaviour
+{
+    public Camera playerCamera;
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit))
+        {
+            Debug.Log(hit.transform.name);
+        }
+    }
+}
--- M2_WeaponSystem/Assets/Scripts/PlayerMovement.cs
+++ M2_WeaponSystem/Assets/Scripts/PlayerMovement.cs
@@ -6,17 +6,30 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController playerController;
     public float speed = 12f;
-    void Start()
-    {
-        
-    }
-
+    Vector3 velocity;
+    public float gravity = -9.81f;
+    public float jumpForce = 5f;
     void Update()
     {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
+        //Movement input
         Vector3 move = transform.right * x + transform.forward * z;
         playerController.Move(move * speed * Time.deltaTime);
+
+        //Gravity control
+        velocity.y += gravity * Time.deltaTime;
+        playerController.Move(velocity * speed * Time.deltaTime);
+
+        if (playerController.isGrounded && velocity.y < 0)
+        {
+            velocity.y = 0f;
+        }
+
+        if (Input.GetButtonDown("Jump") && playerController.isGrounded)
+        {
+            velocity.y = jumpForce;
+        }
     }
 }
